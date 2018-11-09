0eddd5001ec0f4895b3c35a89756bff778754337
George Chamoun <gchamounster123@gmail.com>
2018-11-09 10:42:07 -0600

    test push! commented out graph on homepage

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -2,22 +2,22 @@
 
 <html>
     <head>
-        
+
         <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
 
     </head>
 	<title>Sports Social Rank</title>
 </head>
 <body>
-   
+
 <div class="container">
-<span class="line">0,15,20,10
-</span>
+<!-- <span class="line">0,15,20,10
+</span> -->
 
     </br>
   	<table id="item-list" class="table table-bordered table-striped table-hover" style="width:100%">
 		<thead>
-			<tr>                          
+			<tr>
                             <th>Rank</th>
 
 				<th>Name</th>
