a49eea93068b6498228c1d33c488ca76c77746c1
George Chamoun <gchamounster123@gmail.com>
2018-12-30 18:28:07 -0600

    Fixed registering user to be admin

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -7,7 +7,7 @@
   <link rel="icon" type="image/png" href="../assets/img/favicon.png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
-    Material Dashboard by Creative Tim
+    SportsSocialRank
   </title>
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
   <!--     Fonts and icons     -->
@@ -49,7 +49,7 @@
                 </div>
                 <div style="margin-top:20px;" class="card-body">
                   <?php echo form_open("auth/login");?>
-                <div style="color:red; " id="infoMessage"> 
+                <div style="color:red; " id="infoMessage">
                 <?php echo $message;?></div>
 
 
