77ac00e6808a16f65a6d567525ea73c882c890d5
George Chamoun <gchamounster123@gmail.com>
2019-01-22 07:18:09 -0600

    Added Google Header

--- application/views/templates/aboutheader.php
+++ application/views/templates/aboutheader.php
@@ -23,6 +23,15 @@
 
   <script type="text/javascript" src="js/app.js"></script>
 
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-64395606-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-64395606-2');
+</script>
 
 
 </head>
