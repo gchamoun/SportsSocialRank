0e6807a3169f5eb0f63c96850fc653351ff1276c
George Chamoun <gchamounster123@gmail.com>
2019-01-21 22:57:04 -0600

    mobile table fix

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -10,36 +10,50 @@
           </br></br><em>Followers Today</em> is followers gained since 12:00 am central time of the current day
           </div>
 
-          <div class="row">
 
-<div class="row">
-  <?php foreach ($categories as $category) {
+
+  <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Select Category
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <?php foreach ($categories as $category) {
     ?>
+      <a class="dropdown-item" href="#<?php echo $category->name; ?>"><?php echo $category->name; ?></a>
+
+
+      <?php
+} ?>
+  </div>
+</div>
+
+  <?php foreach ($categories as $category) {
+        ?>
 
             <div class="col-lg-12 col-md-12">
                             <div class="card">
-                <div class="card-header card-header-warning">
+                <div id="<?php echo $category->name; ?>" class="card-header card-header-warning">
                   <h4 class="card-title"><?php echo $category->name; ?></h4>
                 </div>
                 <div class="card-body table-responsive">
 <?php   if ($category->name == "College Football") {
-        $rank[1] = "cfb";
-    }
-    if ($category->name == "College Football") {
-        $category->name = "cfb";
-    }
-    if ($category->name == "NFL Player") {
-        $category->name = "nflplayer";
-    }
-    if ($category->name == "Premier League") {
-        $category->name = "premierleague";
-    }
-    if ($category->name == "College Basketball") {
-        $category->name = "collegebasketball";
-    }
-    if ($category->name == "FCS Football") {
-        $category->name = "FCSFootball";
-    } ?>
+            $rank[1] = "cfb";
+        }
+        if ($category->name == "College Football") {
+            $category->name = "cfb";
+        }
+        if ($category->name == "NFL Player") {
+            $category->name = "nflplayer";
+        }
+        if ($category->name == "Premier League") {
+            $category->name = "premierleague";
+        }
+        if ($category->name == "College Basketball") {
+            $category->name = "collegebasketball";
+        }
+        if ($category->name == "FCS Football") {
+            $category->name = "FCSFootball";
+        } ?>
                   <table id="<?php echo $category->name; ?>-accounts" class="table table-striped " style="width:100%">
                   <thead>
                     <tr>
@@ -66,5 +80,5 @@
         </div>
 
               </div>  <?php
-} ?>
+    } ?>
       </div>
--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -16,7 +16,6 @@
   <title>
     Sports Social Rank
   </title>
-<meta name="viewport" content="width=device-width, initial-scale=1">  <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
   <!-- CSS Files -->
